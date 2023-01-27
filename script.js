2fad1685a3d0a0f6f071266035d60f028154e85d
Ralph Bankston <ralph.l.bankston@gmail.com>
2023-01-26 22:26:30 -0500

    refresher

---
+++ script.js
@@ -0,0 +1,15 @@
+let firstName = "Ralph";
+let lastName = "Bankston";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+let population = 46854;
+console.log(population);
+console.log(population * 3);
+let javaScriptIsCool = true;
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+for ( let i = 0; i <= 5; i++) {
+    console.log(i);
+} 
+for ( let i = 0; i < 5; i++) {
+    console.log(i);
+}
